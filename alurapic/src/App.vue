db2b61e9f0a29ee170c11d1766845813d71455d0
Raphael Giron <maingiron@gmail.com>
2017-03-14 20:47:12 -0300

    Criado componente Painel

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -3,10 +3,9 @@
     <h1 class="text-center">{{ titulo }}</h1>
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotos">
-        <div class="painel">
-          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
-          <div class="painel-conteudo"><img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo"></div>
-        </div>
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
       </li>
     </ul>
   </div>
@@ -52,26 +51,5 @@
 
   .imagem-responsiva {
     width: 100%;
-  }
-
-  .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
   }
 </style>

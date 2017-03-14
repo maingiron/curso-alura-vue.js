4c00ac6c87a790167fe415d3dce0f260a106bb26
Raphael Giron <maingiron@gmail.com>
2017-03-14 20:35:44 -0300

    Melhorado estilização componente com CSS

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -2,7 +2,12 @@
   <div class="corpo">
     <h1 class="text-center">{{ titulo }}</h1>
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos"><img :src="foto.url" :alt="foto.titulo"></li>
+      <li class="lista-fotos-item" v-for="foto of fotos">
+        <div class="painel">
+          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+          <div class="painel-conteudo"><img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo"></div>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
@@ -20,8 +25,8 @@
     created() {
 
       this.$http.get('http://localhost:3000/v1/fotos')
-        .then(res => res.json())
-        .then(fotos => this.fotos = fotos, err => console.log(err));
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
     }
   }
 </script>
@@ -43,5 +48,30 @@
 
   .lista-fotos .lista-fotos-item {
     display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
   }
 </style>

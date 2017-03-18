c98ff1bfd0dfa83137f4d711a6970c4ae74be5e3
Raphael Giron <maingiron@gmail.com>
2017-03-18 14:45:10 -0300

    Criado routes.js e definido as novas rotas

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// Aqui registro os módulos baixados
+// Aqui registro os módulos/rotas baixados
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
+import { routes } from './routes';
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
+const router = new VueRouter({ 
+	// routes: routes --> Em ES6, quando o valor e a propriedade possuem o mesmo nome,
+	// pode simplismente colocar o nome.
+	routes,
+	// Uso o mode history para remover o # da url. 
+	mode: 'history' 
+});
+
 new Vue({
 	el: '#app',
+	router,
 	render: h => h(App)
 })
---
+++ alurapic/src/routes.js
@@ -0,0 +1,11 @@
+import Home from './components/home/Home.vue';
+import Cadastro from './components/cadastro/Cadastro.vue';
+
+export const routes = [
+
+	// O primeiro é a caminho que identifica o componente. 
+	// O segundo o componente que será carregado para este caminho presente na url do navegador.
+    { path: '', component: Home },
+    { path: '/cadastro', component: Cadastro }
+
+];

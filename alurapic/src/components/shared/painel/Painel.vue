cc4137c71958ccd5c076c14a4d23400f9e6d5c5e
Raphael Giron <maingiron@gmail.com>
2017-03-18 13:26:22 -0300

    Incluído comentário explicativo

--- alurapic/src/components/shared/painel/Painel.vue
+++ alurapic/src/components/shared/painel/Painel.vue
@@ -5,6 +5,7 @@
     <!-- O prefixo @ é um atalho para a diretiva v-on. -->
     <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2>
 
+    <!-- Coloco o componente transition para o Vue delimitar a área do template no qual fará a animação. Obrigatóriamente o componente precisa de um name. O componente adiciona dinamicamente nos seus elementos filhos as seguintes classes dinamicamente: [name]-enter, [name]-enter-active, e [name]-leave-active -->
     <transition name="painel-fade">
       <!-- Coloco a tag slot para receber e manter tudo que estiver dentro da tag meu-painel (App.vue). -->
       <!-- A diretiva v-show realiza um display none. Ela não pode ser usada diretamente na tag slot. -->

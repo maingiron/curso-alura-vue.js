2ce90da2a107128523eeba1b7350adfe1abc5943
Raphael Giron <maingiron@gmail.com>
2017-03-18 14:03:27 -0300

    Incluído o VueRouter

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Aqui registro os módulos baixados
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
 	el: '#app',

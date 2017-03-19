cb1231ebed0143d2b5531ee07dd79979a21ea482
Raphael Giron <maingiron@gmail.com>
2017-03-19 12:17:02 -0300

    Incluído mais comportamentos no componente botao

--- alurapic/src/components/shared/botao/Botao.vue
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -1,19 +1,36 @@
 <template>
-	<button @click="disparaAcao()" class="botao botao-perigo" :type="tipo"> {{ rotulo }}</button>
+	<!-- Quando coloco o bind na class o Vue entende que deve concatenar com a class que já existe -->
+	<button @click="disparaAcao()" class="botao" :class="estiloDoBotao" :type="tipo"> {{ rotulo }}</button>
 </template>
 
 <script>
 	export default {
 
-		props: ['tipo', 'rotulo'],
+		props: ['tipo', 'rotulo', 'confirmacao', 'estilo'],
 
 		methods: {
 
 			disparaAcao() {
-				if(confirm('Deseja confirmar a operação?')) {
-					// this.$emit realizar a comunicação entre um componente filho e seu componente pai
+
+				if(this.confirmacao) {
+
+					if(confirm('Deseja confirmar a operação?')) {
+					// this.$emit realizar a comunicação entre um componente filho e seu componente pai.
 					this.$emit('botaoAtivado');
+					}
+					return;
 				}
+				this.$emit('botaoAtivado');
+			}
+		},
+
+		computed: {
+
+			estiloDoBotao() {
+
+				// Verifico se não foi passado o estilo para atribuir o estilo padrão.
+				if(this.estilo == 'padrao' || !this.estilo) return 'botao-padrao';
+				if(this.estilo == 'perigo') return 'botao-perigo';
 			}
 		}
 	}

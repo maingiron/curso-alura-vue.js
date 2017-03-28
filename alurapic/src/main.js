cb6ac75da5b4a8d78e6e075b15ec2762924bceec
Raphael Giron <maingiron@gmail.com>
2017-03-28 20:40:10 -0300

    Instalado o VeeValidate

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -6,11 +6,13 @@ import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import { routes } from './routes';
 import './directives/Transform';
+import VeeValidate from 'vee-validate';
 
 Vue.use(VueResource);
 // O http sempre usará a url abaixo
 Vue.http.options.root = 'http://localhost:3000';
 Vue.use(VueRouter);
+Vue.use(VeeValidate);
 
 const router = new VueRouter({ 
 	// routes: routes --> Em ES6, quando o valor e a propriedade possuem o mesmo nome,

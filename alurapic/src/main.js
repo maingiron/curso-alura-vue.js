678bb7b6c27816f0f2e5da69377095a47243eafd
Raphael Giron <maingiron@gmail.com>
2017-03-28 21:04:29 -0300

    Traduzido as mensagens do VeeValidate para Português

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -7,12 +7,21 @@ import VueRouter from 'vue-router';
 import { routes } from './routes';
 import './directives/Transform';
 import VeeValidate from 'vee-validate';
+import msg from './pt_BR';
 
 Vue.use(VueResource);
 // O http sempre usará a url abaixo
 Vue.http.options.root = 'http://localhost:3000';
 Vue.use(VueRouter);
-Vue.use(VeeValidate);
+Vue.use(VeeValidate, {
+	// Traduz a mensagem de erro do VeeValidate para Português. 
+	locale: 'pt_BR',
+	dictionary: {
+		pt_BR: {
+			messages: msg
+		}
+	}
+});
 
 const router = new VueRouter({ 
 	// routes: routes --> Em ES6, quando o valor e a propriedade possuem o mesmo nome,
